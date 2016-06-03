312ebad0c20cf778dccf6975d18cd833d54ce051
Armand Biteau <armand.biteau@mediamonks.com>
2016-06-03 10:59:14 +0100

    init scene

---
+++ public/src/components/Canvas-experience/objects/ground.js
@@ -0,0 +1,38 @@
+'use strict';
+
+import THREE from 'three';
+
+export default {
+
+    created() {
+
+		// ground
+
+		this._ground = null;
+
+        this._groundColor = 0xf6f6f6;
+
+        this._groundSize = 10;
+
+	},
+
+	methods: {
+
+		groundInitialize() {
+
+            let groundGeometry = new THREE.PlaneGeometry(this._groundSize, this._groundSize);
+            let groundMaterial = new THREE.MeshBasicMaterial({
+                // color: this._groundColor
+            });
+            this._ground = new THREE.Mesh(groundGeometry, groundMaterial);
+
+            this._ground.rotation.x = -Math.PI / 2;
+			this._scene.add(this._ground);
+
+		},
+
+		groundUpdate() {
+
+		}
+	}
+};
