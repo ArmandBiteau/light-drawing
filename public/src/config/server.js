d85d0880673ea98fe037174e8bff4522525bf7f1
Armand Biteau <armand.biteau@mediamonks.com>
2016-06-14 16:39:05 +0100

    update config

--- public/src/config/server.js
+++ public/src/config/server.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const port = process.env.PORT || 5000;
-const ip = process.env.IP || 'localhost';
+
+const ip = (process.env.NOED_ENV != 'production') ? 'localhost' : 'light-drawing.herokuapp.com';
 
 const server = {
 
